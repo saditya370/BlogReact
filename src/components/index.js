c896a7928112c73c24c05efd2a579db32a248484
saditya370 <76547125+saditya370@users.noreply.github.com>
2024-01-05 16:36:09 +0530

    Added Sing up ,Login forms and created components for Postcard , editor

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,31 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import LogoutBtn from "./Header/LogoutBtn";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import Input from "./Input";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import Select from "./Select";
+import Signup from "./Singup";
+import Button from "./Button";
+import AuthLayout from "./AuthLayout";
+import PostCard from "./PostCard";
 
 
 export {
     Header,
-    Footer
+    Footer,
+    LogoutBtn,
+    Container,
+    Logo,
+    Login,
+    PostCard,
+    Signup,
+    Button,
+    AuthLayout,
+    PostCard,
+    Select,
+    Input,
+    
 }
